48b8d86fc5df649823e2cb3290d980af714320d2
Nicky Pochinkov <pochinkn@tcd.ie>
2020-06-22 18:49:33 +0100

    Updated 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,15 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import ParticleBackground from '../components/particle-background'
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  <Layout title="404 Not Found">
+    <ParticleBackground />
+    <div className="full-center-flex" style={{position: "absolute", top:"0px", zIndex: 10, pointerEvents: "none"}}>
+    <h1>PAGE NOT FOUND</h1>
+    <p>You just hit a route that doesn&#39;t exist...</p>
+    </div>
   </Layout>
 )
 

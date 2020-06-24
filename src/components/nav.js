2fa074dd0ca3cbe3364cd4379c3ac598d3183723
Nicky Pochinkov <pochinkn@tcd.ie>
2020-06-24 01:08:33 +0100

    Added background to when navbar scrolls down

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,10 +3,30 @@ import { Link, StaticQuery, graphql } from "gatsby"
 import "../assets/scss/nav.scss"
 import { AnchorLink } from "gatsby-plugin-anchor-links"
 
-const Nav = () => (
+const Nav = () => {
+  
+  if (typeof window !== 'undefined') {
+    let prevScrollpos = window.pageYOffset;
+    window.onscroll = function () {
+      const maxScroll = document.body.clientHeight - window.innerHeight;
+      let currentScrollPos = window.pageYOffset;
+      if (
+          (currentScrollPos < 40)
+        ) {
+        document.getElementById("navbar-container").style.backgroundColor = "transparent";
+        document.getElementById("navbar-container").style.boxShadow = "none";
+      } else {
+        document.getElementById("navbar-container").style.backgroundColor = "#112";
+        document.getElementById("navbar-container").style.boxShadow = "0 2px 2px 0px rgba(255, 255, 255, 0.2)";
+      }
+      prevScrollpos = currentScrollPos;
+    }
+  }
+
+  return (
   <div>
     <div>
-      <nav className="navbar-container" data-uk-navbar>
+      <nav className="navbar-container" id="navbar-container" data-uk-navbar>
         <div className="navbar-left">
           <ul className="navbar-nav">
             <li>
@@ -39,7 +59,7 @@ const Nav = () => (
                 data.allStrapiPage.edges.map((page, i) => {
                   if (page.node.page_list_location.name === "navbar") return (
                     <li key={page.node.strapiId}>
-                      <AnchorLink to={`/${page.node.slug}`} stripHash>
+                      <AnchorLink to={`/${page.node.slug}`} activeClassName="navbar-active" stripHash>
                         {page.node.title}
                       </AnchorLink>
                     </li>
@@ -53,6 +73,6 @@ const Nav = () => (
       </nav>
     </div>
   </div>
-)
+)}
 
 export default Nav

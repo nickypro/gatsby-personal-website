e0c87d246918bcefea80257e44e95d20583be725
Nicky Pochinkov <pochinkn@tcd.ie>
2020-11-15 02:22:16 +0000

    updated 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,11 +2,12 @@ import React from "react"
 
 import Layout from "../components/layout"
 import ParticleBackground from '../components/particle-background'
+import "../assets/scss/main.scss"
 
 const NotFoundPage = () => (
   <Layout title="404 Not Found">
     <ParticleBackground />
-    <div className="full-center-flex" style={{position: "absolute", top:"0px", zIndex: 10, pointerEvents: "none"}}>
+    <div className="light-section full-center-flex" style={{position: "absolute", top:"0px", zIndex: 10, pointerEvents: "none"}}>
     <h1>PAGE NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist...</p>
     </div>

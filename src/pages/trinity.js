e4c99cb77a132701921797055e1e9d1a451bbc62
Nicky Pochinkov <pochinkn@tcd.ie>
2020-11-18 01:37:39 +0000

    removed unused Link import in trinity.js

--- src/pages/trinity.js
+++ src/pages/trinity.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link, StaticQuery, graphql } from "gatsby"
+import { StaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import ParticleBackground from '../components/particle-background'
